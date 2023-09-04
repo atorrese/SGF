ce73e871273adfd155b8831f420d7c6f2e774a00
Angel Torres <atorres@simedcorp.com>
2023-09-04 17:20:59 -0500

    db ma

---
+++ SGF/QueryDb.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE DBPRUEBAS
+CREATE TABLE Contacto(
+	IdContacto INT PRIMARY KEY IDENTITY(1,1),
+	Nombre VARCHAR(100),
+	Apellido VARCHAR(100),
+	Telefono VARCHAR(100),
+	Correo VARCHAR(100)
+)
+SELECT COUNT(*) FROM Contacto
+--truncate table Contacto
